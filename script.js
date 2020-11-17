69ccab3948245ad3429d216ea69d3d19abc6bc67
Alexander Fok <foktmalexander@gmail.com>
2020-11-16 21:48:04 -0800

    Add function for password generation

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Prompt user and return a set of characters
+function getChars() {
+  return [];
+}
+
+// Generate password 
+function generatePassword() {
+  var chars = getChars();
+  console.log("Characters allowed: ", chars);
+
+  return "password";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
