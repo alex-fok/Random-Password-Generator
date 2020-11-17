2a58d2f927e513362061474891aaeead6ef64976
Alexander Fok <foktmalexander@gmail.com>
2020-11-16 21:50:29 -0800

    Set up functions for providing character sets and password length

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ var generateBtn = document.querySelector("#generate");
 function getChars() {
   return [];
 }
+// Prompt user and return a number representing password length
+function getPwdLength() {
+  return 0;
+}
 
 // Generate password 
 function generatePassword() {
   var chars = getChars();
+  var length = getPwdLengths();
   console.log("Characters allowed: ", chars);
+  console.log("Length: ", length);
 
   return "password";
 }
